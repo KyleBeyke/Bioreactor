6b02fff288b123dfa796a0e5a81811333bcbf35e
Kyle Beyke <kyle.beyke@gmail.com>
2024-09-08 00:56:11 -0500

    trying different library

--- dev/code.py
+++ dev/code.py
@@ -1,17 +1,17 @@
 import board
 import busio
-import sdcardio
-import storage
 import digitalio
+import adafruit_sdcard
+import storage
 import os
 
 # SPI configuration for Raspberry Pi Pico
 spi = busio.SPI(board.GP10, board.GP11, board.GP12)
-cs = board.GP13
+cs = digitalio.DigitalInOut(board.GP13)
 
 # Attempt to initialize and mount the SD card
 try:
-    sdcard = sdcardio.SDCard(spi, cs)
+    sdcard = adafruit_sdcard.SDCard(spi, cs)
     vfs = storage.VfsFat(sdcard)
     
     # Check if the mount point directory exists, if not, create it
